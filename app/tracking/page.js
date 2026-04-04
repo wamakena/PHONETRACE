bb4ebc64a5b4f5e1f7e87ca8d6ae158f6cce4106
wamakena <kamauwamakena@gmail.com>
2026-04-04 17:07:24 +0300

    Create page.js

---
+++ app/tracking/page.js
@@ -0,0 +1,11 @@
+export default function Tracking() {
+  return (
+    <div>
+      <h1>Live Tracking</h1>
+
+      <div className="card">
+        <p>Map will appear here</p>
+      </div>
+    </div>
+  );
+}
