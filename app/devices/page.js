a746e8e89d0c7c037f1a8d424310680828e2b8df
wamakena <kamauwamakena@gmail.com>
2026-04-04 17:06:32 +0300

    Create page.js

---
+++ app/devices/page.js
@@ -0,0 +1,11 @@
+export default function Devices() {
+  return (
+    <div>
+      <h1>Registered Devices</h1>
+
+      <div className="card">
+        <p>iPhone 13 - Active</p>
+      </div>
+    </div>
+  );
+}
